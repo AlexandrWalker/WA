e760d6130d35d96b3e213bbc46f394bede356377
AlexandrWalker <alexandr.d.walker@gmail.com>
2025-03-18 19:52:00 +0700

    Updates

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if (navigator.serviceWorker.controller) {
 console.log('[PWA Builder] active service worker found, no need to register')
 } else {
 navigator.serviceWorker.register('/sw.js', {
-scope: 'https://alexandrwalker.github.io/WA/#!'
+scope: 'https://alexandrwalker.github.io/WA/'
 }).then(function(reg) {
 console.log('Service worker has been registered for scope:'+ reg.scope);
 });
